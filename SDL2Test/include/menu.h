19d1f50082455864ff47a25d87b77e7587e33703
Weseung Hwang <weseung+github@gmail.com>
2013-10-03 11:00:20 -0700

    Just some future planning in the comments
    The long term goal is to rework the classes so that instantiating a high
    level class with the appropriate arguments can then instantiate the
    component level ones (sprite, menu) entirely within itself, getting rid
    of a lot of pointers and other dangerous behavior.
    After all, it's perfectly normal to specify which font bitmap you want
    when you instantiate a big module like an input mapper, right? Hmm...

--- SDL2Test/include/menu.h
+++ SDL2Test/include/menu.h
@@ -2,8 +2,10 @@
 #define MENU_H_
 
 //menu.h - manages menu creation, display, and interaction
-//Each menu is a list of strings. Determination of what code to execute is done by
-//reading the integer index of the menu item.
+//Each menu is a list of strings. Selection is done by walking the list.
+//Eventually, this menu class will be instantiated with the ingredients for a given
+//menu by the object that owns the menu instance. Then ExecuteItem would just return the appropriate
+//index and the owning object could react based on that value.
 
 #include "gfxtext.h"
 #include <list>
@@ -15,7 +17,7 @@ public:
 	Menu(BMPText *extrn_TextWriter);
 	void DisplayMenu(int x, int y);
 	void MoveCursor(bool dir);
-	void ExecuteItem();
+	void ExecuteItem();	//Eventually will be returning an int or something so the owning object can use that.
 private:
 	std::list<std::string> rootmenu;
 	std::list<std::string>::iterator itr;
