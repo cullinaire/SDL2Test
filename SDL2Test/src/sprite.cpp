d715e2da0dc43d6039336d4413cf83a2c31817ea
Weseung Hwang <weseung+github@gmail.com>
2013-10-13 03:22:31 -0700

    Added sprite colorkey
    For now the transparent pixel is the color of the one located at 0,0 of
    the sprite sheet. Code taken from
    http://www.libsdl.org/tmp/SDL/test/testsprite2.c

--- SDL2Test/src/sprite.cpp
+++ SDL2Test/src/sprite.cpp
@@ -8,6 +8,30 @@ Sprite::Sprite(const std::string bmpfilename, SDL_Renderer *renderer)
 	image = SDL_LoadBMP(bmpfilename.c_str());
 	if(image != NULL)
 	{
+		//Set transparent pixel as pixel at (0,0)
+		if(image->format->palette)
+		{
+			SDL_SetColorKey(image, 1, *(Uint8 *) image->pixels);
+		}
+		else
+		{
+			switch(image->format->BitsPerPixel)
+			{
+			case 15:
+				SDL_SetColorKey(image, 1, (*(Uint16 *) image->pixels) & 0x00007FFF);
+				break;
+			case 16:
+				SDL_SetColorKey(image, 1, *(Uint16 *) image->pixels);
+				break;
+			case 24:
+				SDL_SetColorKey(image, 1, (*(Uint32 *) image->pixels) & 0x00FFFFFF);
+				break;
+			case 32:
+				SDL_SetColorKey(image, 1, *(Uint32 *) image->pixels);
+				break;
+			}
+		}
+
 		texture = SDL_CreateTextureFromSurface(rend, image);
 		//Surface is freed here; texture will be freed in destructor
 		SDL_FreeSurface(image);
